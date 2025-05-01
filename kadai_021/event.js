69ef437df7b581e036a8a89be29e5656e31e66e2
nakazawadesu <choconamunamu@gmail.com>
2025-05-01 17:39:31 +0900

    Delete kadai_021/event.js

--- kadai_021/event.js
+++
@@ -1,7 +0,0 @@
-const btn = document.getElementById('text');
-const text = document.getElementById('btn');
-
-text.addEventListener('click', () => {
-    setTimeout(
-    btn.textContent = 'ボタンをクリックしました', 2000);
-});
